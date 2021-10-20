1a117ffd8c8a47db39c5db0c00b304facbc08ae2
Nihar Bheemanathi <nbheemanathi@Nihars-MBP.lan>
2021-10-20 11:58:12 -0700

    Updates new url

--- graphql/resolvers/recipes.js
+++ graphql/resolvers/recipes.js
@@ -4,11 +4,11 @@ module.exports = {
   Query: {
     async getRandomRecipes(_, { tags }) {
       const tag = tags[0];
+      const url =  "https://api.spoonacular.com/recipes/random?apiKey=8695dcaaae0c4801b22afda4d5e82429&number=12&tags=" +tag;
+
+      console.log(url)
       try {
-        const response = await fetch(
-          "https://api.spoonacular.com/recipes/random?apiKey=8695dcaaae0c4801b22afda4d5e82429&number=12&tags=" +
-            tag
-        ).then((response) => response.json());
+        const response = await fetch(url).then((response) => response.json());
         return response.recipes;
       } catch (error) {
         throw new Error(error);
