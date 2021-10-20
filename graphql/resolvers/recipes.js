e345c7f3313e9522e664a61b91271ed47ff27592
Nihar Bheemanathi <nbheemanathi@Nihars-MBP.lan>
2021-10-20 10:30:28 -0700

    Updates related to recipes

--- graphql/resolvers/recipes.js
+++ graphql/resolvers/recipes.js
@@ -1,12 +1,12 @@
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
-const { RECIPE_API_KEY } = require("../../config");
+require('dotenv').config();
 
 module.exports = {
     Query: {
         async getRandomRecipes(_, { tags }) {
             try {
               const response = await fetch(
-                `https://api.spoonacular.com/recipes/random?apiKey=${RECIPE_API_KEY}&number=12&tags=${tags.join(
+                process.env.FOOD_API_URL+ `&number=12&tags=${tags.join(
                   ","
                 )}`
               ).then((response) => response.json());
